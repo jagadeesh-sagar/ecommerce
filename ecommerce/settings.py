901525dc72c2f6a94654356c332986de938caec7
G.Jagadeesh <161948918+jagadeesh-sagar@users.noreply.github.com>
2025-11-01 18:54:29 +0530

    Update ALLOWED_HOSTS for production settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!^kgc_w=so@79!s)v!o&#$^2jlmpl4rr7bdug&2xa=a2$nkvwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.234.124.118','localhost']
 
 
 # Application definition
